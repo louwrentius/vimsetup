ff816fceb85533188f682e029c0cbe3d63a0c9c2
louwrentius <louwrentius@gmail.com>
2015-03-28 11:13:20 +0100

    Initial commit

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git
+SAVEPATH=$(pwd)
+DOTVIM=~/.vim
+DOTVIMRC=~/.vimrc
+
+if [ -e $DOTVIM ]
+then
+    echo "Folder $DOTVIM exists, continue? [y/n]"
+    read OVERWRITEDOTVIM
+    if [ $OVERWRITEDOTVIM == "y" ]
+    then
+        mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+        cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git
+    fi
+fi
+
+if [ -e $DOTVIMRC ]
+then
+    echo "File $DOTVIMRC exists, continue [y/n]"
+    read OVERWRITEDOTVIMRC
+    if [ $OVERWRITEDOTVIMRC == "y" ]
+    then
+        cp $SAVEPATH/.vimrc $DOTVIMRC
+    fi
+fi
+
 
